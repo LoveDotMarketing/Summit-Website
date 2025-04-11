cc00ea365e8d405fd71e8dbe48ba617fb0a47427
LoveDotMarketing <lovedotmarketing@gmail.com>
2025-04-11 17:45:59 +0000

    removed link thumbnail

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,19 @@ import routes from "tempo-routes";
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          <Route path="/sponsorship" element={<SponsorshipPage />} />
-          <Route path="/agenda" element={<AgendaPage />} />
-          <Route path="/speakers" element={<SpeakersPage />} />
-          <Route path="/venue" element={<VenuePage />} />
-          {import.meta.env.VITE_TEMPO === "true" && (
-            <Route path="/tempobook/*" element={<div />} />
-          )}
-        </Routes>
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
-      </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="registration" element={<RegistrationPage />} />
+        <Route path="sponsorship" element={<SponsorshipPage />} />
+        <Route path="agenda" element={<AgendaPage />} />
+        <Route path="speakers" element={<SpeakersPage />} />
+        <Route path="venue" element={<VenuePage />} />
+        {import.meta.env.VITE_TEMPO === "true" && (
+          <Route path="tempobook/*" element={<div />} />
+        )}
+      </Routes>
+      {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
     </Suspense>
   );
 }
